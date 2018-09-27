abbe12f9682ddfeb83d7d45afb60b9d877e5ca6e
heberfomin <heber.fomin@gmail.com>
2018-09-27 10:03:31 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://ec2.gruposol.com.br/embarques/api',
+  api_get: 'https://ec2.gruposol.com.br/embarques/api',
+  title: 'Embarques'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_url: '/embarques/api',
+  //api_url: '/embarques/api',
+  api_url: 'https://ec2.gruposol.com.br/embarques/api',
   api_get: 'https://ec2.gruposol.com.br/embarques/api',
   title: 'Embarques'
 };

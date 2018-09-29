5b23d783ae347ca3212bb0dc46dcb6abfd9b5e4c
heberfomin <heber.fomin@gmail.com>
2018-09-29 20:09:05 -0300

    try to cross cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: '/embarques/api',
+  api_url: 'https://ec2.gruposol.com.br/embarques/api',
   api_get: 'https://ec2.gruposol.com.br/embarques/api',
   title: 'Embarques'
 };
